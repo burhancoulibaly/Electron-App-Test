11737005fba601a23de5f81c49faea3878398ff7
burhancoulibaly <burhancoulibaly@gmail.com>
2017-10-24 08:04:31 -0400

    Added top menu for app following tutorial from https://www.youtube.com/watch?v=kN1Czs0m1SU&t=5s

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let home;
 
@@ -15,5 +15,31 @@ app.on('ready', function(){
         pathname: path.join(__dirname, 'home.html'),
         protocol:'file:',
         slashes:true
-    }))
+    }));
+
+    //build menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //adding menu
+    Menu.setApplicationMenu(mainMenu);
 })
+
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label: 'Add Song'
+            },
+            {
+                label: 'Clear Songs'
+            },
+            {
+                label:'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+]
