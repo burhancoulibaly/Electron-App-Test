09bfbd0ed07757110c02c691483a885e1f2c72b4
burhancoulibaly <burhancoulibaly@gmail.com>
2017-10-24 15:00:35 -0400

    finished tutorial packaged app, app can now be ran like a normal desktop application

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+//SET ENV
+process.env.NODE_ENV = 'production';
 
 let home;
+let addWindow;
 
 app.on('ready', function(){
     //Create a window
@@ -17,25 +21,65 @@ app.on('ready', function(){
         slashes:true
     }));
 
+    //quit app when closed
+    home.on('closed', function(){
+        app.quit();
+    });
+
     //build menu
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     //adding menu
     Menu.setApplicationMenu(mainMenu);
 })
 
-const mainMenuTemplate = [
+//create add window
+function createAddWindow(){
+    //Create a window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    
+    //load addwindow html
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol:'file:',
+        slashes:true
+    }));
+
+    //Garbage Collection
+    addWindow.on('close', function(){
+        addWindow = null;
+    })
+}
+
+//catch song:add
+ipcMain.on('song:add',function(e,song){
+    console.log(song);
+    home.webContents.send('song:add', song);
+    addWindow.close();
+})
+
+let mainMenuTemplate = [
     {
-        label:'File',
-        submenu:[
+        label:'File',//main menu label name
+        submenu:[//submenu items below
             {
-                label: 'Add Song'
+                label: 'Add Song',
+                click(){
+                    createAddWindow();
+                }
             },
             {
-                label: 'Clear Songs'
+                label: 'Clear Songs',
+                click(){
+                    home.webContents.send('song:clear');
+                }
             },
             {
                 label:'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',//ternary operator to implement key shortcuts
                 click(){
                     app.quit();
                 }
@@ -43,3 +87,25 @@ const mainMenuTemplate = [
         ]
     }
 ]
+
+//If mac add an empty object to window
+process.platform == 'darwin' ? mainMenuTemplate.unshift({}) : mainMenuTemplate = mainMenuTemplate;
+
+//add developer tools item if not in prod
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label:'Developer Tools',
+        submenu:[
+            {
+                label:'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',//ternary operator to implement key shortcuts 
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
